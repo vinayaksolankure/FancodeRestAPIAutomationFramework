40d6bd27d631837cd015e1e36627d1ff8cbc931c
Vinayak Solankure <vinayaksolankure9284@gmail.com>
2024-09-29 11:23:51 +0530

    Implemented Extent report code

--- src/test/java/fancode/tests/TestRunner.java
+++ src/test/java/fancode/tests/TestRunner.java
@@ -4,10 +4,11 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-	    features = "src/test/resources/features", // Path to the feature files
-	    glue = {"fancode.tests"}, // Path to the step definitions
-	    plugin = {"pretty", "html:target/cucumber-reports"} // Reporting
-	)
+		features = "src/test/resources/features", // Path to the feature files
+		glue = {"fancode.tests"}, // Path to the step definitions
+		//plugin = {"pretty","junit:target/cucumber-reports/report_xml.xml","json:target/cucumber-reports/report_json.json","html:target/cucumber-reports/report_html.html"}
+		plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+		)
 
 public class TestRunner extends AbstractTestNGCucumberTests{
 }
